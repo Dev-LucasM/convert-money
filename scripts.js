c11481b0c5d2b115c08942c2ae2cc44c90e99de0
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-02-22 09:08:42 -0300

    scripts melhorados

--- scripts.js
+++ scripts.js
@@ -1,58 +1,50 @@
-const button = document.getElementById("convert-button");
-const select = document.getElementById("currency-select");
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
 
-const dolar = 5.2;
-const euro = 5.6;
-const bitcoin = 118000
+const dolar = 5.2
+const euro = 5.5
+const bitcoin = 124000
 
-const convertValues = () => {
-  const inputReais = document.getElementById("input-real").value;
-  const realValueText = document.getElementById("real-value-text");
-  const currencyValueText = document.getElementById("currency-value-text");
+convertValues = () => {
+  const inputReal = document.getElementById("input-real").value
+  const realValueText = document.getElementById("real-value-text")
+  const currencyValueText = document.getElementById("currency-value-text")
 
   realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(inputReais);
+  }).format(inputReal)
 
   if (select.value === "US$ Dólar americano") {
     currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
-    }).format(inputReais / dolar);
-  }
-
-  if (select.value === "€ Euro") {
+    }).format(inputReal / dolar)
+  } else if (select.value === "€ Euro") {
     currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "EUR",
-    }).format(inputReais / euro);
+    }).format(inputReal / euro)
+  } else {
+    currencyValueText.innerHTML = (inputReal / bitcoin).toFixed(7)
   }
-
-  if (select.value === "Bitcoin") {
-    currencyValueText.innerHTML = (inputReais / bitcoin).toFixed(7)
-}
 }
 
-changeCurrency = () => {
-  const currencyName = document.getElementById("currency-name");
-  const currencyImg = document.getElementById("currency-img");
+changeElements = () => {
+  const currencyName = document.getElementById("currency-name")
+  const currencyImg = document.getElementById("currency-img")
 
   if (select.value === "US$ Dólar americano") {
-    currencyName.innerHTML = "Dólar americano";
-    currencyImg.src = "./assets/estados-unidos.svg";
-  }
-
-  if (select.value === "€ Euro") {
-    currencyName.innerHTML = "Euro";
-    currencyImg.src = "./assets/euro.svg";
-  }
-
-  if (select.value === "Bitcoin") {
+    currencyName.innerHTML = "Dólar americano"
+    currencyImg.src = "./assets/estados-unidos.svg"
+  } else if (select.value === "€ Euro") {
+    currencyName.innerHTML = "Euro"
+    currencyImg.src = "./assets/euro.svg"
+  } else {
     currencyName.innerHTML = "Bitcoin"
     currencyImg.src = "./assets/bitcoin.png"
   }
-};
+}
 
-button.addEventListener("click", convertValues);
-select.addEventListener("change", changeCurrency);
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeElements)
